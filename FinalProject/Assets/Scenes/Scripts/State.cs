95fd5f4b8eb2a4e7b3991985d23cc4560ef4abf1
Abhishek Panhale <ap8093@rit.edu.in>
2020-11-30 03:32:44 -0500

    Initial commit

---
+++ FinalProject/Assets/Scenes/Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected EnemyAI enemy;
+
+    public State(EnemyAI enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public virtual void OnEnter() { }
+
+    public virtual void UpdateState() { }
+
+    public virtual void OnExit() { }
+}
